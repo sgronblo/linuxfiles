f9d0a1b8c7820597f3fa45d5345968222018e451
Sam Grönblom <sgronblo@gmail.com>
2010-07-02 01:09:01 +0300

    Fixed symlink creation script
    - removed the echo before the ln
    - only link .dotfiles except for .git

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 LINUX_FILES_PATH=~/linuxfiles
-find $LINUX_FILES_PATH -mindepth 1 -maxdepth 1 -printf '%f\0' | xargs -0rI{} echo ln -Tfs $LINUX_FILES_PATH/{} ~/{}
+find $LINUX_FILES_PATH -mindepth 1 -maxdepth 1 -name ".*" \! -name ".git" -printf '%f\0' | xargs -0rI{} ln -Tfs $LINUX_FILES_PATH/{} ~/{}
